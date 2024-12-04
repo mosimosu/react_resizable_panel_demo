298e5e7878d8b2af9d66ce24d10e628e0d622e80
Mosi <mosisu07@gmail.com>
2024-12-04 15:26:48 +0800

    feat: added vertical example

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import { Direction } from "./components/ResizableCard/ResizableCardEnum"
 function App() {
   return (
     <>
+    {/* 水平範例 */}
       <ResizableCard 
-        // 設置面板的方向：Vertical 為垂直排列，Horizontal 為水平排列
         direction={Direction.Horizontal} 
         
         cards={[
           {
-            content: <div ><h1>Card 1</h1></div>,
+            children: <div ><h1>Card 1</h1></div>,
             defaultSize: 30,
             minSize: 20,
             style: {
@@ -22,7 +22,7 @@ function App() {
             
           },
           {
-            content: <div ><h1>Card 2</h1></div>,
+            children: <div ><h1>Card 2</h1></div>,
             defaultSize: 30,
             minSize: 20,
             style: {
@@ -30,21 +30,82 @@ function App() {
               alignItems: 'center',
               justifyContent: 'center',
             },
+            handle:{
+              style: {
+                border: '1px solid #ccc',
+              }
+            }
           },
           {
-            content: <div ><h1>Card 3</h1></div>,
+            children: <div ><h1>Card 3</h1></div>,
             defaultSize: 40,
             minSize: 20,
             style: {
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center',
+            },
+            handle:{
+              style: {
+                border: '1px solid #ccc',
+              }
             }
           }
         ]}
         style={{
           width:'1000px',
-          height:'400px',
+          height:'100px',
+          border: '1px solid #ccc',
+        }}
+      />
+      {/* 垂直範例 */}
+      <ResizableCard 
+        direction={Direction.Vertical} 
+        cards={[
+          {
+            children: <div ><h1>Card 1</h1></div>,
+            defaultSize: 30,
+            minSize: 20,
+            style: {
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }
+          },
+          {
+            children: <div ><h1>Card 2</h1></div>,
+            defaultSize: 30,
+            minSize: 20,
+            style: {
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+            handle:{
+              style: {
+                border: '1px solid #ccc',
+              }
+            }
+          },
+          {
+            children: <div ><h1>Card 3</h1></div>,
+            defaultSize: 40,            
+            minSize: 20,
+            style: {
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            },
+            handle:{
+              style: {
+                border: '1px solid #ccc',
+              }
+            }
+          }
+        ]}
+        style={{
+          width:'500px',
+          height:'700px',
           border: '1px solid #ccc',
         }}
       />
